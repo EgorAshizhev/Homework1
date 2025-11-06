cab4207d5f639e3400d2728baa5ced820363a751
Egor <egor.ashizhev.1997@mail.ru>
2025-11-06 15:26:43 +0300

    исправил замечания

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-// exercise 1
-{
-    let a = 10;
-    alert(a);
-    a = 20;
-    alert(a);
-}
-// exercise 2
-const iPhone = 2007;
-alert(iPhone)
-//  exercise 3
-let JavaCreator = "Brendan Eich";
-alert(JavaCreator)
-// exercise 4
-{
-    let a = 10;
-    let b = 2;
+// // exercise 1
+// {
+//     let a = 10;
+//     alert(a);
+//     a = 20;
+//     alert(a);
+// }
+// // exercise 2
+// const iPhone = 2007;
+// alert(iPhone)
+// //  exercise 3
+// let JavaCreator = "Brendan Eich";
+// alert(JavaCreator)
+// // exercise 4
+// {
+//     let a = 10;
+//     let b = 2;
 
-    alert("Сумма: " + (a + b));
-    alert("Разность: " + (a - b));
-    alert("Произведение: " + (a * b));
-    alert("Частное: " + (a / b));
-    //  exercise 5
-    let result = 2 ** 5;
-    alert(result)
-}
-// exercise 6
-{
-    let a = 9;
-    let b = 2;
-    let remainder = a % b;
-    alert("Остаток от деления " + a + " на " + b + " равен " + remainder);
-}
-// exercise 7
-{
-    let num = 1;
-    num += 5;
-    num -= 3;
-    num *= 7;
-    num /= 3;
-    num++;
-    num--;
-    alert(num);
-}
-// exercise 8
-let age = prompt('Сколько вам лет?');
-alert(age);
-// exercise 9
-const user = {
-    name: "Tadeo",
-    age: 28,
-    isAdmin: true
-};
-// exercise 10
-const userName = prompt("Пожалуйста, введите ваше имя:");
-alert(`Привет, ${userName}!`);
+//     alert("Сумма: " + (a + b));
+//     alert("Разность: " + (a - b));
+//     alert("Произведение: " + (a * b));
+//     alert("Частное: " + (a / b));
+//     //  exercise 5
+//     let result = 2 ** 5;
+//     alert(result)
+// }
+// // exercise 6
+// {
+//     let a = 9;
+//     let b = 2;
+//     let remainder = a % b;
+//     alert("Остаток от деления " + a + " на " + b + " равен " + remainder);
+// }
+// // exercise 7
+// {
+//     let num = 1;
+//     num += 5;
+//     num -= 3;
+//     num *= 7;
+//     num /= 3;
+//     num++;
+//     num--;
+//     alert(num);
+// }
+// // exercise 8
+// let age = prompt('Сколько вам лет?');
+// alert(age);
+// // exercise 9
+// const user = {
+//     name: "Tadeo",
+//     age: 28,
+//     isAdmin: true
+// };
+// // exercise 10
+// const userName = prompt("Пожалуйста, введите ваше имя:");
+// alert(`Привет, ${userName}!`);
